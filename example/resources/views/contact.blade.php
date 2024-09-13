db165c4933da60cd427e5a66be53e50a4832f3c7
estellemsastro <estelle2007-sastropawiro@outlook.com>
2024-09-13 14:27:59 +0200

    heb video 4 afgemaakt en ben nu bij video 5,5

--- example/resources/views/contact.blade.php
+++ example/resources/views/contact.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Contact page
+    </x-slot:heading>
 <h1>hello from the contact Page.</h1>
 </x-layout>
--- example/resources/views/home.blade.php
+++ example/resources/views/home.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        home page
+    </x-slot:heading>
+    
 <h1>hello from the Home Page.</h1>
 </x-layout>
